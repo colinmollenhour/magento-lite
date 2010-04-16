794c14e512d52f59159673e8304156d1a8d99ec4
Colin <colin@mollenhour.com>
2010-04-16 12:43:52 -0400

    Fix broken Widget module by adding dummy file for product types

---
+++ app/code/community/Mage/Catalog/Model/Product/Type.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Satisfies app/code/core/Mage/Widget/Model/Widget/Instance.php on line 76
+ */
+class Mage_Catalog_Model_Product_Type {
+
+  public static function getTypes()
+  {
+    return array();
+  }
+
+}
