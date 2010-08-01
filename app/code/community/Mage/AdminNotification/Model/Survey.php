c7d79d0bc3b87fe2472da8cca1504aba2da55693
Colin <colin@mollenhour.com>
2010-07-31 23:43:24 -0400

    Fix error on last page of installer introduce by update to 1.4.1.1

--- app/code/community/Mage/AdminNotification/Model/Survey.php
+++ app/code/community/Mage/AdminNotification/Model/Survey.php
@@ -1,9 +1,18 @@
 <?php
 /**
- * Used to satisfy app/code/core/Mage/Install/controllers/WizardController.php:432
+ * Used to satisfy:
+ *   app/code/core/Mage/Install/controllers/WizardController.php:435
+ *   app/code/core/Mage/Install/Block/End.php:57
  */
 class Mage_AdminNotification_Model_Survey {
 
-  public static function saveSurveyViewed($foo){}
-  
+  public static function saveSurveyViewed($foo)
+  {
+  }
+
+  public static function isSurveyUrlValid()
+  {
+    return FALSE;
+  }
+
 }
