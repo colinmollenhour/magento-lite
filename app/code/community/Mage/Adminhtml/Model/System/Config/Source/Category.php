7b3fde3c19e747aaad8487ecd1d2f34e2f96e0c9
Colin <colin@mollenhour.com>
2011-02-10 14:43:57 -0500

    Adding model for category source to fix broken "Edit Store" form.

---
+++ app/code/community/Mage/Adminhtml/Model/System/Config/Source/Category.php
@@ -0,0 +1,12 @@
+<?php
+
+class Mage_Adminhtml_Model_System_Config_Source_Category
+{
+
+  public function toOptionArray()
+  {
+    return array(array('value' => '1', 'label'  => '---'));
+  }
+
+}
+
