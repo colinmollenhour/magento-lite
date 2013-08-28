294c2b5386bb1d5384daea4f197f8957669405ee
Colin Mollenhour <colin@mollenhour.com>
2013-08-27 23:58:28 -0400

    Bump copyright year first to make diff more useful.

--- lib/Varien/Event/Observer/Regex.php
+++ lib/Varien/Event/Observer/Regex.php
@@ -22,6 +22,7 @@
  * @package    Varien_Event
  * @copyright  Copyright (c) 2008 Irubin Consulting Inc. DBA Varien (http://www.varien.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+>>>>>>> Bump copyright year first to make diff more useful.:app/code/community/Mage/Compiler/controllers/ProcessController.php
  */
 
 
