dde64dd92e91993bf40a29a1f262d0cf5fd02263
Colin Mollenhour <colin@mollenhour.com>
2015-11-05 11:17:50 -0500

    Merge more changes from v1.9.2.2, delete some unneeded files.

--- lib/Mage/Connect/Command/Config_Header.php
+++ lib/Mage/Connect/Command/Config_Header.php
@@ -21,7 +21,7 @@
  * @category    Mage
  * @package     Mage_Connect
  * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
 $commands = array(
--- lib/Varien/Convert/Action/Interface.php
+++ lib/Varien/Convert/Action/Interface.php
@@ -18,8 +18,8 @@
  * versions in the future. If you wish to customize Magento for your
  * needs please refer to http://www.magento.com for more information.
  *
- * @category   Varien
- * @package    Varien_Convert
+ * @category    Varien
+ * @package     Varien_Convert
  * @copyright  Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
@@ -39,5 +39,5 @@ interface Varien_Convert_Action_Interface
      *
      * @return Varien_Convert_Action_Abstract
      */
-	public function run();
+    public function run();
 }
