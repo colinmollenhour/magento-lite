fe5eeea302eb1caa65fbe021bc954bf0c584b4ca
Lucas van Staden <lucas@dhemdia.com.au>
2015-11-11 16:54:56 +0800

    add stub class Mage_Catalog_Model_Category to satisfy need for cache_tag const

---
+++ app/code/community/Mage/Catalog/Model/Category.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Satisfies Mage_Page_Block_Html_Topmenu::_construct needing CACHE_TAG
+ */
+class Mage_Catalog_Model_Category extends Mage_Core_Model_Abstract
+{
+    const CACHE_TAG = 'catalog_category';
+}
